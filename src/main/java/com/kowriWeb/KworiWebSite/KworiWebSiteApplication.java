70d0d7eb2bed76b3def411bca96cdbfed4c2f047
Cyril-dot <chukwukaottih@gmail.com>
2026-03-30 14:21:11 +0000

    update

--- src/main/java/com/kowriWeb/KworiWebSite/KworiWebSiteApplication.java
+++ src/main/java/com/kowriWeb/KworiWebSite/KworiWebSiteApplication.java
@@ -1,5 +1,6 @@
 package com.kowriWeb.KworiWebSite;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class KworiWebSiteApplication {
 
 	public static void main(String[] args) {
+
+		Dotenv dotenv = Dotenv.configure()
+				.directory("./")       // looks for .env in the project root (next to pom.xml)
+				.ignoreIfMissing()     // safe in CI/CD environments without .env
+				.load();
+
+		dotenv.entries().forEach(entry ->
+				System.setProperty(entry.getKey(), entry.getValue())
+		);
+
 		SpringApplication.run(KworiWebSiteApplication.class, args);
 	}
 
